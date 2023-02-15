88153d8de3c50491262b432c8e1dd8820f92ca3f
julia <julia.albq@gmail.com>
2023-02-15 16:26:05 -0300

    feat: add useInput to PunsForm logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
+import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { PunsForm } from "./components/PunsForm/PunsForm";
 import { PunsList } from "./components/PunsList/PunsList";
+import { useInput } from "./hooks/useInput";
 import { puns } from "./puns/puns";
 
 function App() {
+  const [formValues, setFormValues] = useState({});
+
+  const {
+    value: date,
+    setValue: setDate,
+    handleInput: handleDate,
+  } = useInput(formValues.date ?? "00/00/00");
+
+  const {
+    value: dev,
+    setValue: setDev,
+    handleInput: handleDev,
+  } = useInput(formValues.dev ?? "");
+
+  const {
+    value: context,
+    setValue: setContext,
+    handleInput: handleContext,
+  } = useInput(formValues.dev ?? "");
+
+  const {
+    value: pun,
+    setValue: setPun,
+    handleInput: handlePun,
+  } = useInput(formValues.pun ?? "");
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const elements = event.target.elements;
+
+    const newPun = {
+      date: elements.date.value,
+      dev: elements.dev.value,
+      context: elements.dev.value,
+      pun: elements.pun.value,
+    };
+
+    setFormValues(newPun);
+    console.log(newPun);
+  };
+
   return (
     <div className="App">
       <Header />
       <PunsList puns={puns} />
-      <PunsForm />
+      <PunsForm
+        date={date}
+        handleDate={handleDate}
+        dev={dev}
+        handleDev={handleDev}
+        context={context}
+        handleContext={handleContext}
+        pun={pun}
+        handlePun={handlePun}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
--- src/components/PunsForm/PunsForm.jsx
+++ src/components/PunsForm/PunsForm.jsx
@@ -1,27 +1,65 @@
 import "./PunsForm.css";
 
-export const PunsForm = () => {
+export const PunsForm = ({
+  date,
+  handleDate,
+  dev,
+  handleDev,
+  context,
+  handleContext,
+  pun,
+  handlePun,
+  handleSubmit,
+}) => {
   return (
     <div className="form__wrapper">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2 className="title">Add New Pun</h2>
         <div className="form__division">
           <label className="form__label" name="date">
             Date:
-            <input className="form__input" type={Date} id="date" />
+            <input
+              className="form__input"
+              type="date"
+              id="date"
+              required
+              value={date}
+              onChange={handleDate}
+            />
           </label>
           <label className="form__label" name="dev">
             Dev:
-            <input className="form__input" type={Text} id="dev" />
+            <input
+              className="form__input"
+              type="text"
+              id="dev"
+              required
+              value={dev}
+              onChange={handleDev}
+            />
           </label>
         </div>
         <label className="form__label" name="context">
           Context:
-          <input className="form__input" type={Text} id="context" />
+          <input
+            className="form__input"
+            type="text"
+            id="context"
+            required
+            value={context}
+            onChange={handleContext}
+          />
         </label>
         <label className="form__label" name="pun">
           Pun:
-          <input className="form__input" type={Text} id="pun" />
+          <input
+            className="form__input"
+            type="text"
+            id="pun"
+            required
+            value={pun}
+            onChange={handlePun}
+          />
         </label>
         <button className="form__button" type="submit">
           Add Pun
