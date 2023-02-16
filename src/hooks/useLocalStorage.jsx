c7c955015b43bbd4255017a00a4a6d341b5548c0
julia <julia.albq@gmail.com>
2023-02-16 16:31:16 -0300

    refactor: refactor hook useLocalStorage to storage the array of puns

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const useLocalStorage = (key, defaultValue) => {
-    const [value, setValue] = useState(defaultValue)
-    const [allValues, setAllvalues] = useState([])
-    const saved = localStorage.getItem(key);
-    JSON.parse(saved)
+  const [value, setValue] = useState(defaultValue);
+  const [allValues, setAllvalues] = useState([]);
+  const saved = localStorage.getItem(key);
+  const parsed = JSON.parse(saved);
 
   function saveValue(value) {
-    setValue(value)
-    const updatedAllValues = [...allValues, value]
-    setAllvalues(updatedAllValues)
-    window.localStorage.setItem(`${key}`, JSON.stringify(allValues))
+    setValue(value);
+
+    const updatedValues = [...allValues, value];
+    setAllvalues(updatedValues);
+
+    window.localStorage.setItem(`${key}`, JSON.stringify(updatedValues));
   }
 
   function resetValue(key) {
-      window.localStorage.removeItem(key)
-      setValue(defaultValue)  
+    window.localStorage.removeItem(key);
+    setValue(defaultValue);
   }
 
-  return { value, saveValue, resetValue };
-}
+  return { value, parsed, saveValue, resetValue };
+};
